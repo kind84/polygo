4460f0ccc68a3b9dbce342a21f9aa6d7bc7df21f
Paolo Grisoli <paolo_g84@icloud.com>
2019-08-19 18:48:56 +0200

    Adds redis script test

--- translator/main_test.go
+++ translator/main_test.go
@@ -24,12 +24,12 @@ func TestRedisConsumerGroup(t *testing.T) {
 	rh := viper.GetString("redis.host")
 	rdb := redis.NewClient(&redis.Options{Addr: rh})
 	defer rdb.Close()
+
 	stream := "storyblok-test"
 	group := "translate-test"
 	consumer := "translator-test"
-	rdb.XGroupCreate(stream, group, "$")
+	rdb.XGroupCreate(stream, group, "$").Result()
 
-	// id := "test-1"
 	msg := map[string]interface{}{"Hello": "World"}
 
 	argsAdd := &redis.XAddArgs{
@@ -40,11 +40,7 @@ func TestRedisConsumerGroup(t *testing.T) {
 		Values: msg,
 	}
 
-	// pipe := rdb.Pipeline()
-	// defer pipe.Close()
-
 	id, err := rdb.XAdd(argsAdd).Result()
-	// c, err := pipe.Exec()
 	if err != nil {
 		t.Error(err)
 	}
@@ -88,4 +84,125 @@ func TestRedisConsumerGroup(t *testing.T) {
 	}
 
 	rdb.XDel(stream, id).Result()
+}
+
+func TestRedisScript(t *testing.T) {
+	rh := viper.GetString("redis.host")
+	rdb := redis.NewClient(&redis.Options{Addr: rh})
+	defer rdb.Close()
+
+	streamFrom := "storyblok-test"
+	streamTo := "translation-test"
+	groupFrom := "translate-test"
+	groupTo := "storybloks-test"
+	consumerFrom := "translator-test"
+	consumerTo := "storybloker-test"
+	rdb.XGroupCreate(streamFrom, groupFrom, "$").Result()
+	rdb.XGroupCreate(streamTo, groupTo, "$").Result()
+
+	msgFrom := map[string]interface{}{"Hello": "World"}
+
+	argsAdd := &redis.XAddArgs{
+		Stream: streamFrom,
+		// MaxLen       int64 // MAXLEN N
+		// MaxLenApprox int64 // MAXLEN ~ N
+		// ID:     id,
+		Values: msgFrom,
+	}
+
+	idFrom, err := rdb.XAdd(argsAdd).Result()
+	if err != nil {
+		t.Error(err)
+	}
+
+	argsReadFrom := &redis.XReadGroupArgs{
+		Group:    groupFrom,
+		Consumer: consumerFrom,
+		// List of streams and ids.
+		Streams: []string{streamFrom, ">"},
+		// Count   int64
+		// Block: time.Millisecond * 2000,
+		// NoAck   bool
+	}
+
+	itemsFrom := rdb.XReadGroup(argsReadFrom)
+	if itemsFrom == nil {
+		t.Error("Error reading streamFrom")
+		// rdb.XDel(stream, id)
+		return
+	}
+	fmt.Println(itemsFrom.Val())
+	if len(itemsFrom.Val()) == 0 || len(itemsFrom.Val()[0].Messages) == 0 {
+		t.Error("Empty streamFrom")
+		// rdb.XDel(stream, id)
+		return
+	}
+
+	tStreamFrom := itemsFrom.Val()[0]
+	v := tStreamFrom.Messages[0].Values["Hello"].(string)
+	if v != "World" {
+		t.Errorf("Error reading from stream: got %s, want %s", v, "World")
+		return
+	}
+
+	ackNaddScript := redis.NewScript(`
+		if redis.call("xack", KEYS[1], ARGV[1], ARGV[2]) == 1 then
+			return redis.call("xadd", KEYS[2], "*", ARGV[3], ARGV[4])
+		end
+		return false
+	`)
+
+	idTo, err := ackNaddScript.Run(
+		rdb,
+		[]string{streamFrom, streamTo}, // KEYS
+		[]string{groupFrom, idFrom, "Hi", "Redis"}, // ARGV
+	).Result()
+
+	if err != nil {
+		t.Error(err)
+	}
+	// if r != 1 {
+	// 	t.Errorf("Acknowledge of message id %v failed", id)
+	// }
+
+	argsReadTo := &redis.XReadGroupArgs{
+		Group:    groupTo,
+		Consumer: consumerTo,
+		// List of streams and ids.
+		Streams: []string{streamTo, ">"},
+		// Count   int64
+		// Block: time.Millisecond * 2000,
+		// NoAck   bool
+	}
+
+	itemsTo := rdb.XReadGroup(argsReadTo)
+	if itemsTo == nil {
+		t.Error("Error reading streamTo")
+		// rdb.XDel(stream, id)
+		return
+	}
+	fmt.Println(itemsTo.Val())
+	if len(itemsTo.Val()) == 0 || len(itemsTo.Val()[0].Messages) == 0 {
+		t.Error("Empty streamTo")
+		// rdb.XDel(stream, id)
+		return
+	}
+
+	tStreamTo := itemsTo.Val()[0]
+	v = tStreamTo.Messages[0].Values["Hi"].(string)
+	if v != "Redis" {
+		t.Errorf("Error reading from stream: got %s, want %s", v, "Redis")
+		return
+	}
+
+	r, err := rdb.XAck(streamTo, groupTo, idTo.(string)).Result()
+	if err != nil {
+		t.Error(err)
+	}
+	if r != 1 {
+		t.Errorf("Acknowledge of message id %v failed", idTo)
+	}
+
+	rdb.XDel(streamFrom, idFrom).Result()
+	rdb.XDel(streamFrom, idTo.(string)).Result()
 }
